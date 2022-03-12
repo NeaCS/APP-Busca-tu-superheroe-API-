1c5de40db8f1e8c4a6c50cc059987de0d8675dfa
NeaCS <naldi.castro@gmail.com>
2022-03-12 00:30:32 -0300

    card with information ready

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -2,6 +2,7 @@ $(() => {
   let buscar = $("#botonbuscar");
 
   buscar.on("click", () => {
+    
     let personaje = $("#numeroPersonaje").val();
     $.ajax({
       type: "GET",
@@ -9,10 +10,19 @@ $(() => {
       dataType: "JSON",
       success: (dataApi) => {
         console.log("salida de dataApi", dataApi);
-        let tarjeta = $("#card");
+        let tarjeta = $(".card");
         tarjeta.html(`
-                <h2>${dataApi.name}</h2>
-                <img class="card-img-top" src="${dataApi.image.url}" alt="Card image cap">`);
+        <img src="${dataApi.image.url}" alt="imagensuperheros">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item">Nombre: ${dataApi.name}</br>
+        Conexiones: ${dataApi.connections[`group-affiliation`]}</li>
+        <li class="list-group-item">Publicado por: ${dataApi.biography.publisher}</li>
+        <li class="list-group-item">Ocupación: ${dataApi.work.occupation}</li>
+        <li class="list-group-item">Primera aparición: ${dataApi.biography[`first-appearance`]}</li>
+        <li class="list-group-item">Altura: ${dataApi.appearance.height}</li>
+        <li class="list-group-item">Peso: ${dataApi.appearance.weight}</li>
+        <li class="list-group-item">Alianzas: ${dataApi.biography.aliases}</li>
+      </ul>`);
       },
 
       error: () => {
