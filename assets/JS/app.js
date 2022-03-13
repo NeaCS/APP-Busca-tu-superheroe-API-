3a59fb5f079783beb5817fd7426392a051b84403
NeaCS <naldi.castro@gmail.com>
2022-03-13 12:01:02 -0300

    integración exitosa de CANVAJS

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,15 +1,45 @@
 $(() => {
   let buscar = $("#botonbuscar");
 
+  let dataPoints = [];
+
+  var options = {
+    title: {
+      text: "Poderes del superheroe"
+    },
+    data: [{
+      type: "pie",
+      startAngle: 45,
+      showInLegend: "true",
+      legendText: "{label}",
+      indexLabel: "{label} ({y})",
+      yValueFormatString: "#,##0.#" % "",
+      dataPoints: dataPoints
+    }]
+  };
+
   buscar.on("click", () => {
-    
+
     let personaje = $("#numeroPersonaje").val();
+
     $.ajax({
       type: "GET",
       url: `https://www.superheroapi.com/api.php/4905856019427443/${personaje}`,
       dataType: "JSON",
       success: (dataApi) => {
         console.log("salida de dataApi", dataApi);
+        for (prop in dataApi.powerstats) {
+          dataPoints.push({
+            label: prop,
+            y: dataApi.powerstats[prop]
+          })
+
+        }
+        $("#chartContainer").CanvasJSChart(options);
+        dataPoints = []
+
+
+
         let tarjeta = $(".card");
         tarjeta.html(`
         <img src="${dataApi.image.url}" alt="imagensuperheros">
@@ -23,6 +53,8 @@ $(() => {
         <li class="list-group-item">Peso: ${dataApi.appearance.weight}</li>
         <li class="list-group-item">Alianzas: ${dataApi.biography.aliases}</li>
       </ul>`);
+
+
       },
 
       error: () => {
