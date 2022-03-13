b530132cad3e867d98bc9f0e60ab312470fdb8fb
NeaCS <naldi.castro@gmail.com>
2022-03-13 14:47:17 -0300

    proyecto completo

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,11 +1,10 @@
 $(() => {
-  let buscar = $("#botonbuscar");
 
-  let dataPoints = [];
 
+  var dataPoints = [];
   var options = {
     title: {
-      text: "Poderes del superheroe",
+      text: 'Poderes del superheroe'
     },
     data: [
       {
@@ -20,8 +19,15 @@ $(() => {
     ],
   };
 
+  let buscar = $("#botonbuscar");
+
+
   buscar.on("click", () => {
     let personaje = $("#numeroPersonaje").val();
+    //Validar si el valor ingresado es un número
+    if (isNaN(personaje)) {
+      alert("Por favor, escriba un número válido")
+    } else {
 
     $.ajax({
       type: "GET",
@@ -29,37 +35,44 @@ $(() => {
       dataType: "JSON",
       success: (dataApi) => {
         console.log("salida de dataApi", dataApi);
-        for (prop in dataApi.powerstats) {
-          dataPoints.push({
-            label: prop,
-            y: dataApi.powerstats[prop],
-          });
-        }
-        $("#chartContainer").CanvasJSChart(options);
-        dataPoints = [];
 
-        let tarjeta = $(".card");
+        let tarjeta = $("#cardContainer");
         tarjeta.html(`
+        <section class="card" style="width: 30rem;">
+            
         <img src="${dataApi.image.url}" alt="imagensuperheros">
         <ul class="list-group list-group-flush">
         <li class="list-group-item">Nombre: ${dataApi.name}</br>
         Conexiones: ${dataApi.connections[`group-affiliation`]}</li>
-        <li class="list-group-item">Publicado por: ${
-          dataApi.biography.publisher
-        }</li>
+        <li class="list-group-item">Publicado por: ${dataApi.biography.publisher
+          }</li>
         <li class="list-group-item">Ocupación: ${dataApi.work.occupation}</li>
-        <li class="list-group-item">Primera aparición: ${
-          dataApi.biography[`first-appearance`]
-        }</li>
+        <li class="list-group-item">Primera aparición: ${dataApi.biography[`first-appearance`]
+          }</li>
         <li class="list-group-item">Altura: ${dataApi.appearance.height}</li>
         <li class="list-group-item">Peso: ${dataApi.appearance.weight}</li>
         <li class="list-group-item">Alianzas: ${dataApi.biography.aliases}</li>
-      </ul>`);
+      </ul></section>`);
+
+ // Bucle for in para rellenar información del gráfico
+        for (prop in dataApi.powerstats) {
+
+          dataPoints.push({
+            label: prop,
+            y: dataApi.powerstats[prop],
+          });
+
+        } console.log(dataPoints)
+        
+        $("#chartContainer").CanvasJSChart(options);
+        // vaciar el array para volver a llenarlo con la info nueva
+        dataPoints.length = 0
+
       },
 
       error: () => {
         alert(`Hubo un error. Por favor, inténtelo más tarde`);
       },
     });
-  });
+  }});
 });
